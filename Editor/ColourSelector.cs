abed0d0d9d854fdcddee270669f0483eb8edfb35
Ophura <8chan.com@gmail.com>
2024-12-30 02:38:37 +0100

    Update ColourSelector.cs

--- Editor/ColourSelector.cs
+++ Editor/ColourSelector.cs
@@ -7,17 +7,14 @@ namespace Ophura
 {
     internal static class ColourSelector
     {
-        internal static readonly Type ColorPicker;
         private static readonly MethodInfo ShowFunction;
         private static readonly object[] Arguments = { null, null, true, false };
 
         static ColourSelector()
         {
-            ColorPicker = AccessTools.TypeByName(Consistent.ColorPickerFullTypeName);
-
             Type[] Signature = { typeof(Action<Color>), typeof(Color), typeof(bool), typeof(bool) };
 
-            ShowFunction = AccessTools.Method(type: ColorPicker, name: nameof(Show), parameters: Signature);
+            ShowFunction = AccessTools.Method(type: Consistent.ColorPicker, name: nameof(Show), parameters: Signature);
         }
 
         internal static void Show(Action<Color> Operation, Color InitialColour)
@@ -28,6 +25,6 @@ namespace Ophura
             ShowFunction.Invoke(obj: null, parameters: Arguments);
         }
 
-        internal static void OnDestroy() => ColourStorage.instance.Lodge();
+        internal static void OnDestroy() => ColourStorage.Singular.Lodge();
     }
 }
