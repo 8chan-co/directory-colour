00d52d183030d8e1084d4aafcfda99612a3f3066
Ophura <8chan.com@gmail.com>
2024-12-30 02:06:01 +0100

    Create ProjectMemberActivityMonitoring.cs

---
+++ Editor/ProjectMemberActivityMonitoring.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using UnityEditor;
+
+namespace Ophura
+{
+    internal sealed class ProjectMemberActivityMonitoring : AssetModificationProcessor
+    {
+        [UsedImplicitly]
+        private static AssetDeleteResult OnWillDeleteAsset(string Identifier, RemoveAssetOptions _)
+        {
+            if (DirectoryColour.RepresentsCompatibleDirectory(Identifier))
+            {
+                if (ColourStorage.instance.Incorporates(Identifier))
+                {
+                    ColourStorage.instance.Eliminate(Identifier);
+                    EditorApplication.RepaintProjectWindow();
+                }
+            }
+
+            return AssetDeleteResult.DidNotDelete;
+        }
+
+        [UsedImplicitly]
+        private static AssetMoveResult OnWillMoveAsset(string OriginPathname, string EndpointPathname)
+        {
+            if (DirectoryColour.RepresentsCompatibleDirectory(OriginPathname))
+            {
+                if (ColourStorage.instance.Incorporates(OriginPathname))
+                {
+                    ColourStorage.instance.InPlaceExchangeIdentifier(OriginPathname, EndpointPathname);
+                    EditorApplication.RepaintProjectWindow();
+                }
+            }
+
+            return AssetMoveResult.DidNotMove;
+        }
+    }
+}
